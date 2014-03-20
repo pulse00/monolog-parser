ae180ead5bbedbdf5622a341416c54cf9577fada
Hugo Seabra <hugoseabra@faeg.com.br>
2014-03-20 15:43:15 -0300

    Correcting native pattern

--- src/Dubture/Monolog/Parser/LineLogParser.php
+++ src/Dubture/Monolog/Parser/LineLogParser.php
@@ -13,7 +13,7 @@ namespace Dubture\Monolog\Parser;
 
 class LineLogParser implements LogParserInterface
 {
-    protected $pattern = '/\[(?P<date>.*)\] (?P<logger>\w+).(?P<level>\w+): (?P<message>[^ ]+) (?P<context>[^ ]+) (?P<extra>[^ ]+)/';
+    protected $pattern = '/\[(?P<date>.*)\] (?P<logger>\w+).(?P<level>\w+): (?P<message>.*[^ ]+) (?P<context>[^ ]+) (?P<extra>[^ ]+)/';
     
     /**
      * Constructor
